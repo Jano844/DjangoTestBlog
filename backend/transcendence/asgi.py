b703651a5f5aef0013673588841540a8bbfa379d
Jonstep101010 <mail@jschwabe.site>
2024-11-26 10:17:29 +0100

    chore: rename project

--- backend/transcendence/asgi.py
+++ backend/transcendence/asgi.py
@@ -1,5 +1,5 @@
 # """
-# ASGI config for django_test project.
+# ASGI config for transcendence project.
 
 # It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@
 
 # from django.core.asgi import get_asgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_test.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
 
 # application = get_asgi_application()
 
